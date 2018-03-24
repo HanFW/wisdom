05ef4bf9da85a525a7cd8e6e63439b6106ad1ee9
Yongxue <yongxue0701@gmail.com>
2018-03-25 00:18:28 +0800

    add new article - sessionbean, managedbean, web page

--- Wisdom-ejb/src/java/sessionBean/ArticleSessionBean.java
+++ Wisdom-ejb/src/java/sessionBean/ArticleSessionBean.java
@@ -7,6 +7,7 @@ package sessionBean;
 
 import entity.Article;
 import entity.Author;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,19 +18,24 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class ArticleSessionBean implements ArticleSessionBeanLocal {
-    
+
+    @EJB(name = "AuthorSessionBeanLocal")
+    private AuthorSessionBeanLocal authorSessionBeanLocal;
+
     @PersistenceContext
     private EntityManager entityManager;
-    
+
     @Override
-    public Long addNewArticle(String topic, String title, String description, 
-            String context, Author author) {
+    public Long addNewArticle(String topic, String title, String description,
+            String context, Long authorId) {
+
+        Author author = authorSessionBeanLocal.retrieveAuthorById(authorId);
         
         Article article = new Article(topic, title, description, context, author);
-        
+
         entityManager.persist(article);
         entityManager.flush();
-        
+
         return article.getArticleId();
     }
 }
