7217e0da4e74783cf1dbc6dfe01fa0eca59b9e56
Yongxue <yongxue0701@gmail.com>
2018-03-24 20:32:51 +0800

    create session bean and managed bean - article

---
+++ Wisdom-war/src/java/managedBean/AddNewArticleManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Yongxue
+ */
+@Named(value = "addNewArticleManagedBean")
+@RequestScoped
+public class AddNewArticleManagedBean {
+
+    /**
+     * Creates a new instance of AddNewArticleManagedBean
+     */
+    public AddNewArticleManagedBean() {
+    }
+    
+}
